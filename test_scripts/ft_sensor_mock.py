2382eeb0b4ca4e132e9b939a028b08734c720039
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2023-07-20 12:51:48 +0200

    fixed naming bug + added possbility to change simulation with service

--- test_scripts/ft_sensor_mock.py
+++ test_scripts/ft_sensor_mock.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 import argparse
 import itertools
+from turtlesim.srv import TeleportAbsolute
+
+from functools import partial
 
 
 def rep_list_until(ll: list, l: int):
@@ -21,7 +24,7 @@ def rep_list_until(ll: list, l: int):
 
 
 class FT_Mockup:
-    header = [f"{f}{dim}" for dim in "xyz" for f in "FM"]
+    header = [f"{f}{dim}" for f in "FM" for dim in "xyz"]
 
     def __init__(
         self,
@@ -34,6 +37,8 @@ class FT_Mockup:
         self.run_flag = True
         self.freq = freq
         self.iterators = []
+        self.no_repeats = len(topics)
+        self.df = []
 
         f_m_l = rep_list_until(f_m, len(topics) * 3)
         f_r_l = rep_list_until(f_r, len(topics) * 3)
@@ -43,21 +48,39 @@ class FT_Mockup:
                 nn = np.zeros((2, len(self.header)))
                 nn[0, 0:3] = np.array(f_m_l[i * 3 : (i + 1) * 3])
                 nn[1, 0:3] = np.array(f_r_l[i * 3 : (i + 1) * 3])
-                df = pd.DataFrame(nn, columns=self.header, index=["m", "r"])
-                self.create_random_replay_file(df)
+                self.df.append(pd.DataFrame(nn, columns=self.header, index=["m", "r"]))
+                self.create_random_replay_file(self.df[-1])
         else:
             self.load_replay_file(replay_file)
 
         # TODO make this a rosparam
         rospy.init_node("FT_Sensor", anonymous=True)
         # TODO: Make this a rosparam
-        # self.publisher = rospy.Publisher("wrench_test", WrenchStamped, queue_size=10)
         self.publishers = [
             rospy.Publisher(n, WrenchStamped, queue_size=10) for n in topics
         ]
 
+        self.sv_prox = [
+            rospy.Service(
+                f"/change_element/F{a}",
+                TeleportAbsolute,
+                partial(self.service, element=a),
+            )
+            for a in range(0, self.no_repeats)
+        ]
+
         while True:
             self.publish_data()
+
+    def service(self, data, element=0):
+        x = data.x
+        y = data.y
+        z = data.theta
+        fml = [x, y, z]
+        self.df[element].loc[["m"], ["Fx", "Fy", "Fz"]] = fml
+
+        self.create_random_replay_file(self.df[element], no=element)
+        return []
 
     def load_replay_file(self, replay_file: str):
         """load a file to be replayed from csv
@@ -76,8 +99,10 @@ class FT_Mockup:
         except FileNotFoundError:
             return False
 
-    def create_random_replay_file(self, df):
+    def create_random_replay_file(self, df, no=None):
         sec = 100
+
+        # self.iterators = []
 
         ma = np.array([df.loc["m"]])
         ra = np.array([df.loc["r"]])
@@ -93,7 +118,10 @@ class FT_Mockup:
             columns=self.header,
         )
         self.replay_set = data
-        self.iterators.append(itertools.cycle(self.replay_set.iterrows()))
+        if no is None:
+            self.iterators.append(itertools.cycle(self.replay_set.iterrows()))
+        else:
+            self.iterators[no] = itertools.cycle(self.replay_set.iterrows())
 
     def create_new_wrench_msg(self, it):
         w_msg = WrenchStamped()
